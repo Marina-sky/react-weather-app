922b68146cd04bb1d9a070ba219396b8de26b96e
Marina-sky <75041628+Marina-sky@users.noreply.github.com>
2021-08-21 17:49:47 +0200

    API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-import HumidityWind from "./components/HumidityWind";
 import SearchEngine from "./components/SearchEngine";
-import Temperature from "./components/Temperature";
 import Footer from "./components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="App">
-      <div className="container">
-        <div className="card">
-          <div className="card-body">
-            <SearchEngine />
-            <HumidityWind />
-            <Temperature />
+  const [weatherData, setweatherData] = useState({ ready: false });
+  function showData(response) {
+    console.log(response.data);
+    setweatherData({
+      ready: true,
+      city: response.data.name,
+      date: "Saturday, 17:00",
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      iconUrl: "https://openweathermap.org/img/w/01d.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="card">
+            <div className="card-body">
+              <SearchEngine />
+              <div className="row">
+                <div className="col">
+                  <h1 className="city">{weatherData.city}</h1>
+                </div>
+                <div className="col">
+                  <p className="humidity-wind">
+                    Humidity: {weatherData.humidity}% <br />
+                    Wind: {Math.round(weatherData.wind)} km/h
+                  </p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <p className="today">
+                    {weatherData.date} <br />
+                    <span className="current-weather">
+                      {weatherData.description}
+                    </span>
+                    <br />
+                    <img
+                      src={weatherData.iconUrl}
+                      alt={weatherData.description}
+                      className="current-icon"
+                    />
+                    <span className="current-temperature">
+                      {Math.round(weatherData.temperature)}
+                    </span>
+                    <span className="units">
+                      <a href="/" className="active">
+                        °C
+                      </a>{" "}
+                      |<a href="/">°F</a>
+                    </span>
+                  </p>
+                </div>
+                <div className="col">
+                  <p className="quote">
+                    It is a great day for naps. Followed by naps.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ba32fdef5d635b26f61cd641f6ab56c1";
+    let city = "Berlin";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showData);
+    return "Loading...";
+  }
 }
